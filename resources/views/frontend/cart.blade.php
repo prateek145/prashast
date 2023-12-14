34cea3d9656b26a2a331cee4d8e5b77f198c30ee
prateek145 <prateekk898@gmail.com>
2023-12-14 18:31:53 +0530

    local

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -10,8 +10,9 @@
                 <div class="col-md-5 col-lg-4 order-md-last">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-primary">Your cart</span>
-                        <span class="badge bg-primary rounded-pill">2</span>
+                        {{-- <span class="badge bg-primary rounded-pill">2</span> --}}
                     </h4>
+                    @if (session()->get('cart'))
                     <ul class="list-group mb-3">
                         @php
                             $totalprice = 0;
@@ -35,6 +36,8 @@
                             <strong>{{ $totalprice }}</strong>
                         </li>
                     </ul>
+                        
+                    @endif
 
                 </div>
                 <div class="col-md-7 col-lg-8">
