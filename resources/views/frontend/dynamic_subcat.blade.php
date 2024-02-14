001e1c412f332461e25d7333e9749bb570988573
prateek145 <prateekk898@gmail.com>
2024-02-14 14:30:59 +0530

    local

--- resources/views/frontend/dynamic_subcat.blade.php
+++ resources/views/frontend/dynamic_subcat.blade.php
@@ -114,9 +114,7 @@
                             @csrf
                             <input type="text" class=" border-0 form-control" onkeyup="searchproducts(this.value)"
                                 list="datalistname" placeholder="Search">
-                            <select id="datalistname" class="form-control ml-2">
-                                <option value="">Search & Select</option>
-                            </select>
+                            <select id="datalistname" class="form-control ml-2"></select>
                             <button class="btn border-0 bg-light"><i class="bi bi-search"></i></button>
                             <input type="hidden" name="id" id="mainsearchbox">
                         </form>
