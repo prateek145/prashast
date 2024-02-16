d1bb9353325e07eee0ecaab17d3e55133667c075
prateek145 <prateekk898@gmail.com>
2024-02-16 18:20:40 +0530

    loca

--- resources/views/vendor/binshopsblog/index.blade.php
+++ resources/views/vendor/binshopsblog/index.blade.php
@@ -1,18 +1,11 @@
-@extends("frontend.layouts.app",['title'=>$title])
-
-@section('blog-custom-css')
-    <link type="text/css" href="{{ asset('public/binshops-blog.css') }}" rel="stylesheet">
-@endsection
-
-@section("content")
-
+@extends('frontend.layouts.app', ['title' => $title])
+@section('content')
     <div class='col-sm-12 binshopsblog_container'>
-        @if(\Auth::check() && \Auth::user()->canManageBinshopsBlogPosts())
+        @if (\Auth::check() && \Auth::user()->canManageBinshopsBlogPosts())
             <div class="text-center">
                 <p class='mb-1'>You are logged in as a blog admin user.
                     <br>
-                    <a href='{{route("binshopsblog.admin.index")}}'
-                       class='btn border  btn-outline-primary btn-sm '>
+                    <a href='{{ route('binshopsblog.admin.index') }}' class='btn border  btn-outline-primary btn-sm '>
                         <i class="fa fa-cogs" aria-hidden="true"></i>
                         Go To Blog Admin Panel</a>
                 </p>
@@ -22,33 +15,33 @@
         <div class="row">
             <div class="col-md-9">
 
-                @if($category_chain)
+                @if ($category_chain)
                     <div class="container">
                         <div class="row">
                             <div class="col-md-12">
                                 @forelse($category_chain as $cat)
-                                    / <a href="{{$cat->categoryTranslations[0]->url($locale)}}">
-                                        <span class="cat1">{{$cat->categoryTranslations[0]['category_name']}}</span>
+                                    / <a href="{{ $cat->categoryTranslations[0]->url($locale) }}">
+                                        <span class="cat1">{{ $cat->categoryTranslations[0]['category_name'] }}</span>
                                     </a>
-                                @empty @endforelse
+                                @empty
+                                @endforelse
                             </div>
                         </div>
                     </div>
                 @endif
 
-                @if(isset($binshopsblog_category) && $binshopsblog_category)
-                    <h2 class='text-center'> {{$binshopsblog_category->category_name}}</h2>
+                @if (isset($binshopsblog_category) && $binshopsblog_category)
+                    <h2 class='text-center'> {{ $binshopsblog_category->category_name }}</h2>
 
-                    @if($binshopsblog_category->category_description)
-                        <p class='text-center'>{{$binshopsblog_category->category_description}}</p>
+                    @if ($binshopsblog_category->category_description)
+                        <p class='text-center'>{{ $binshopsblog_category->category_description }}</p>
                     @endif
-
                 @endif
 
                 <div class="container">
                     <div class="row mb-2">
                         @forelse($posts as $post)
-                            @include("binshopsblog::partials.index_loop")
+                            @include('binshopsblog::partials.index_loop')
                         @empty
                             <div class="col-md-12">
                                 <div class='alert alert-danger'>No posts!</div>
@@ -59,39 +52,43 @@
             </div>
             <div class="col-md-3 bg-light p-2">
                 <div class="card p-2">
-                            @if (config('binshopsblog.search.search_enabled') )
-            @include('binshopsblog::sitewide.search_form')
-        @endif
+                    @if (config('binshopsblog.search.search_enabled'))
+                        @include('binshopsblog::sitewide.search_form')
+                    @endif
                 </div>
                 <hr>
                 <h2>Blog Categories</h2>
                 <ul class="binshops-cat-hierarchy">
-                    @if($categories)
-                        @include("binshopsblog::partials._category_partial", [
-    'category_tree' => $categories,
-    'name_chain' => $nameChain = "",
-    'routeWithoutLocale' => $routeWithoutLocale
-    ])
+                    @if ($categories)
+                        @include('binshopsblog::partials._category_partial', [
+                            'category_tree' => $categories,
+                            'name_chain' => ($nameChain = ''),
+                            'routeWithoutLocale' => $routeWithoutLocale,
+                        ])
                     @else
                         <span>No Categories</span>
                     @endif
                 </ul>
-                
-                
-                
+
+
+
             </div>
         </div>
 
 
         <div class="row d-none">
             <div class="col-md-12 text-center">
-                @foreach($lang_list as $lang)
-                    <a href="{{route("binshopsblog.index" , $lang->locale)}}">
-                        <span>{{$lang->name}}</span>
+                @foreach ($lang_list as $lang)
+                    <a href="{{ route('binshopsblog.index', $lang->locale) }}">
+                        <span>{{ $lang->name }}</span>
                     </a>
                 @endforeach
             </div>
         </div>
     </div>
 
+@endsection
+
+@section('blog-custom-css')
+    <link type="text/css" href="{{ asset('public/binshops-blog.css') }}" rel="stylesheet">
 @endsection
--- resources/views/vendor/binshopsblog/partials/index_loop.blade.php
+++ resources/views/vendor/binshopsblog/partials/index_loop.blade.php
@@ -6,18 +6,19 @@
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col-auto d-none d-lg-block">
-           {{-- {{dd($post->image_tag("medium", true, ''))}} --}} <!--Remove atag-->
+           {{-- {{dd($post->image_tag("medium", true, ''))}} <!--Remove atag--> --}}
             <?=$post->image_tag("medium", true, ''); ?>
           
         </div>
+        {{dd($post)}}
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary-emphasis">{{$post->category_name}}ghjgjhg</strong> <!--NOT working-->
+          {{-- <strong class="d-inline-block mb-2 text-primary-emphasis">{{$post->categories}}asdasd</strong> <!--NOT working--> --}}
           <h3 class="mb-0">{{$post->title}}</h3>
           <div class="mb-1 text-body-secondary">{{$post->subtitle}}</div>
           
          
           
-          <a href='{{$post->url($locale, $routeWithoutLocale)}}' class="icon-link gap-1 icon-link-hover stretched-link">
+          <a href='{{$post->url($locale, $routeWithoutLocale)}}' class="icon-link gap-1 icon-link-hover">
             Continue reading
             <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
           </a>
