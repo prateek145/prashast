cc8d2dd370c700f5349368027f6943c50fb9116d
prateek145 <prateekk898@gmail.com>
2024-03-20 19:31:05 +0530

    local

--- resources/views/frontend/dynamic_subcat.blade.php
+++ resources/views/frontend/dynamic_subcat.blade.php
@@ -120,7 +120,7 @@
                             {!! $fsidebar->description !!}
                         @endif
 
-                        <h2 style="padding: 1rem">Filter</h2>
+                        {{-- <h2 style="padding: 1rem">Filter</h2>
                         <form action="{{ route('shop.page') }}" method="get" id="filter_form">
                             <input type="hidden" name="category" value="{{request()->segment(2) ?? request()->input('category')}}">
                             <input type="hidden" name="filter_search">
@@ -130,7 +130,7 @@
                                 <option value="{{ $medium_price }}">{{ $medium_price }} to {{ $max_price }}</option>
                                 <option value="{{ $max_price }}">{{ $max_price }} ></option>
                             </select>
-                        </form>
+                        </form> --}}
 
                     </div>
                 </div>
